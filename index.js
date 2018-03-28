cdda19f610ea5877a0ef4a7a650efd37466b9de8
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-28 18:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-function iterativeLog(array)
-{
-  array.forEach((element,index,array)=>{console.log(`${index}: ${element}`}))
+function iterativeLog(array){
+  array.forEach((element,index) => {console.log(`${index}: ${element}`)})
 }
 
-function iterate(callback)
-{
+function iterate(callback){
   let array=[1,2,3]
   array.forEach(callback);
   return array;
 }
 
-index doToArray(array,callback){
+function doToArray(array,callback){
   array.forEach(callback);
 }
