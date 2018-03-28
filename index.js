aab623b955f2fa2873b381df9a7901e61878aa4d
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-28 18:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function iterate(callback)
   return array;
 }
 
-function doToArray(array,callback)
-{
-  array.forEach(callback)
+function doToArray(array,callback){
+  array.forEach(callback);
 }
+
